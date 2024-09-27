965f136d22759142f89e2f8bbbf0c5b866004a69
maxrahul7 <rahul.chauhan7@maxlifeinsurance.com>
2024-09-27 17:51:55 +0530

    before codemode

--- config/server.js
+++ config/server.js
@@ -1,7 +1,9 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '738f8c8bdd5b28d210598105242784e5'),
+    },
   },
 });
